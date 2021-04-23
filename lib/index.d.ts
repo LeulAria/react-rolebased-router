f6e7cd6d73715ab59b3ab706afba8c98a65f2c9e
Leul <leulsegedtariku@gmail.com>
2021-04-23 11:48:54 +0300

    ⬆️ DOWN DEP: rm optional router fields type

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -2,9 +2,9 @@ import React from "react";
 import { RouterConfig, IBlocked } from "./types";
 interface IProps {
     routes: RouterConfig[];
-    userAuthRoles?: string[];
-    loginRedirectPath?: string;
-    isUserAuthenticated?: boolean;
+    userAuthRoles: string[];
+    loginRedirectPath: string;
+    isUserAuthenticated: boolean;
     blocked?: IBlocked;
 }
 declare const Router: React.FC<IProps>;
