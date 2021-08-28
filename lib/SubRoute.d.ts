db5b9d095c27cc81ac05243fb5bd9085650ae4e3
Leul <leulsegedtariku@gmail.com>
2021-08-28 10:12:50 +0300

    👌 UPDATE: update on props build

--- lib/SubRoute.d.ts
+++ lib/SubRoute.d.ts
@@ -1,10 +1,12 @@
 import React from "react";
+import { RouteProps } from "react-router-dom";
 import { IBlocked } from "./types";
 interface IProps {
     userAuthRoles: string[];
     loginRedirectPath: string;
     isUserAuthenticated: boolean;
     blocked?: IBlocked;
+    routeProps: RouteProps;
     [route: string]: any;
 }
 declare const SubRoute: React.FC<IProps>;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,11 +1,15 @@
 import React from "react";
+import { SwitchProps } from "react-router-dom";
 import { RouterConfig, IBlocked } from "./types";
+export * from './types';
+export * from './SubRoute';
 interface IProps {
     routes: RouterConfig[];
     userAuthRoles?: string[];
     loginRedirectPath?: string;
     isUserAuthenticated?: boolean;
     blocked?: IBlocked;
+    switchProps: SwitchProps;
 }
 declare const Router: React.FC<IProps>;
 export default Router;
